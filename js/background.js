e2e70b5204cb7263dea98802e8d3f3cb2bce6d62
Danilo Novakovic <dakenzi97@gmail.com>
2018-09-21 15:14:20 +0200

    added bookmarks.onRemoved listener in background.js

--- js/background.js
+++ js/background.js
@@ -1,8 +1,46 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (!changeInfo.url) return;
 
-  // get bookmarks
-  // chrome.storage.sync.get(['dynBookmarks'], function(result) {
+  const newUrl = changeInfo.url;
 
-  // });
+  // get bookmarks
+  chrome.storage.sync.get(['dynBookmarks'], ({ dynBookmarks }) => {
+    let dynBook = dynBookmarks || {};
+    for (let id in dynBook) {
+      let { url, regExp } = dynBook[id];
+      try {
+        regExp = new RegExp(regExp);
+      } catch {
+        console.warn(
+          `regExp: ${regExp} from dynBookmarks.id of ${id} is invalid...`
+        );
+        continue;
+      }
+      if (url !== newUrl && regExp.test(newUrl)) {
+        console.log(`Updating bookmark with id of ${id} to url: ${newUrl}`);
+        chrome.bookmarks.update(id, { url: newUrl }, () => {
+          if (chrome.runtime.lastError) {
+            console.warn(chrome.runtime.lastError.message);
+          }
+        });
+      }
+    }
+  });
+});
+
+chrome.bookmarks.onRemoved.addListener((id) => {
+  chrome.storage.sync.get(['dynBookmarks'], ({ dynBookmarks }) => {
+    let dynBook = dynBookmarks || {};
+    if (dynBook[id]) {
+      delete dynBook[id];
+      console.log(
+        `Successfully removed bookmark with id of ${id} from storage`
+      );
+      chrome.storage.sync.set({ dynBookmarks: dynBook }, () => {
+        if (chrome.runtime.lastError) {
+          console.warn(chrome.runtime.lastError.message);
+        }
+      });
+    }
+  });
 });
