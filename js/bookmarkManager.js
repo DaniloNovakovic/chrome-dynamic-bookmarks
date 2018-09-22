c827d72de58d0455fb83047ec3e6d1b7142ac50a
Danilo Novakovic <dakenzi97@gmail.com>
2018-09-22 17:29:23 +0200

    added simple tree react-like structure

--- js/bookmarkManager.js
+++ js/bookmarkManager.js
@@ -7,8 +7,39 @@ const File = ({ name }) => {
   );
 };
 
+function changeOpened(event) {
+  //const folderHeader = event.target;
+}
+
+const Folder = (props, ...children) => {
+  const opened = props.opened || false;
+  const arrowIcon = opened ? 'arrow_drop_down' : 'arrow_right';
+  const folderIcon = opened ? 'folder_open' : 'folder';
+  const folderItems = children.map((child, index) => {
+    li(child);
+  });
+  const folderName = props.name || 'unknown';
+  return div(
+    { className: 'folder' },
+    header(
+      {
+        onClick: changeOpened,
+        className: 'folder-header'
+      },
+      i({ className: 'material-icons' }, arrowIcon),
+      i({ className: 'material-icons' }, folderIcon),
+      span(null, folderName)
+    ),
+    ul({ className: opened ? '' : 'hide' }, ...folderItems)
+  );
+};
+
 const TreeView = () => {
-  return section({ className: 'container' }, File({ name: 'myFile.js' }));
+  return section(
+    { className: 'container' },
+    File({ name: 'myBookmark.js' }),
+    Folder({ name: 'otherBookmarks' })
+  );
 };
 
 const app = document.querySelector('#treeView');
