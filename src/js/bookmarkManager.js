bca7c705c1c801d754fad287595d2825cc27cdff
Danilo Novakovic <dakenzi97@gmail.com>
2018-09-28 23:16:55 +0200

    added additional png imports in bookmarkManager and popup.js

--- src/js/bookmarkManager.js
+++ src/js/bookmarkManager.js
@@ -1,28 +1,16 @@
-/*
-  <link rel="stylesheet" href="./css/material-icons.css">
-  <link rel="stylesheet" href="./css/materialize.min.css">
-  <link rel="stylesheet" href="./css/treeView.css">
-  <link rel="stylesheet" href="./css/bookmarkInfo.css">
-  <link rel="stylesheet" href="./css/manager.css">
-*/
+// images
+import '../images/default_favicon.png';
+import '../images/icons8_Books_48.png';
+import '../images/icons8_Books_64.png';
+
+// css
 import '../css/material-icons.css';
 import '../css/materialize.min.css';
 import '../css/treeView.css';
 import '../css/bookmarkInfo.css';
 import '../css/manager.css';
 
-/*
-  <script src="./js/materialize.min.js"></script>
-  <script src="./js/react-clone.js"></script>
-  <script src="./js/selectHandler.js"></script>
-  <script src="./js/searchBar.js"></script>
-  <script src="./js/bookmarkInfo.js"></script>
-  <script src="./js/folderInfo.js"></script>
-  <script src="./js/treeViewComponents.js"></script>
-  <script src="./js/treeView.js"></script>
-  <script src="./js/managerForm.js"></script>
-  <script src="./js/manager.js"></script>
-*/
+// js
 import './lib/materialize.min.js';
 import './lib/react-clone.js';
 import './bookmarkManager/selectHandler.js';
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,3 +1,10 @@
+// images
+import '../images/icons8_Books_16.png';
+import '../images/icons8_Books_32.png';
+import '../images/icons8_Books_48.png';
+import '../images/icons8_Books_64.png';
+import '../images/icons8_Books_128.png';
+
 // styles
 import '../css/material-icons.css';
 import '../css/materialize.min.css';
