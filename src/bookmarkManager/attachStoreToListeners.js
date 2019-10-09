dd629f614b43004ad7bd949bb800d9cf1d72dc35
Danilo Novakovic <dakenzi97@gmail.com>
2019-10-09 10:29:18 +0200

    finished writing connectToBackground function

--- src/bookmarkManager/attachStoreToListeners.js
+++ src/bookmarkManager/attachStoreToListeners.js
@@ -2,6 +2,6 @@ import connectToBackground from "./connectToBackground";
 import { getBookmarkNodes } from "shared/store/actions";
 
 export default function attachStoreToListeners(store) {
-  connectToBackground();
   store.dispatch(getBookmarkNodes());
+  connectToBackground();
 }
--- src/bookmarkManager/connectToBackground.js
+++ src/bookmarkManager/connectToBackground.js
@@ -2,9 +2,28 @@ import { getCurrentBrowser } from "shared/lib/browser";
 
 const browser = getCurrentBrowser();
 
-export default function connectToBackground() {
+function log(msg) {
+  console.log(msg);
+}
+
+/**
+ * @callback onMessageCallback
+ * @param {{type: string, data: any}} message
+ * @param {function} sendResponse
+ */
+
+/**
+ * Adds listeners for messages from background page, calls `onMessage` when a new message is received.
+ * @param {onMessageCallback} onMessage - callback function called when message is received from background
+ */
+export default function connectToBackground(onMessage = log) {
   const port = browser.runtime.connect({ name: "bookmarks" });
+
+  function sendResponse(res) {
+    port.postMessage(res);
+  }
+
   port.onMessage.addListener(msg => {
-    console.log(msg);
+    onMessage(msg, sendResponse);
   });
 }
