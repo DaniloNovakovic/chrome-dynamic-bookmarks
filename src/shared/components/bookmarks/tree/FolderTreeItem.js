e91d1402f74dc1c9c3db1153f7485629c89c29ed
Danilo Novakovic <dakenzi97@gmail.com>
2019-10-19 14:07:23 +0200

    Fixed bug where parent folder would not expand in tree view on breadcrumb-link click

--- src/shared/components/bookmarks/tree/FolderTreeItem.js
+++ src/shared/components/bookmarks/tree/FolderTreeItem.js
@@ -12,41 +12,38 @@ import {
 } from "shared/store/selectors/index";
 import TreeItem from "./TreeItem";
 
-const defaultChildren = [];
-
 export function FolderTreeItem({
   node,
   applyFilter,
   selected = false,
-  initExpanded = false,
-  children = defaultChildren
+  breadcrumbIds,
+  children
 }) {
   const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
-    if (initExpanded) {
+    if (_isAncestor(breadcrumbIds, node.id)) {
       setExpanded(true);
     }
-  }, [initExpanded]);
+  }, [breadcrumbIds]);
 
   function toggleExpanded() {
     setExpanded(!expanded);
   }
+
   function handleClick() {
     if (!selected) {
-      applyFilter({ parentId: id });
+      applyFilter({ parentId: node.id });
     }
   }
 
-  const { id, title: labelText } = node || {};
-
   return (
     <TreeItem
-      labelText={labelText}
-      expandIcon={expanded ? ExpandMoreIcon : ChevronRightIcon}
-      labelIcon={expanded ? FolderOpenIcon : FolderIcon}
       selected={selected}
       expanded={expanded}
+      expandIcon={expanded ? ExpandMoreIcon : ChevronRightIcon}
+      labelIcon={expanded ? FolderOpenIcon : FolderIcon}
+      labelText={node.title}
       toggleExpanded={toggleExpanded}
       onClick={handleClick}
       children={children}
@@ -66,7 +63,7 @@ function _isAncestor(breadcrumbIds = [], id = "0") {
 function mapStateToProps(state, { node }) {
   return {
     selected: filterSelector(state).parentId === node.id,
-    initExpanded: _isAncestor(breadcrumbIdsSelector(state), node.id)
+    breadcrumbIds: breadcrumbIdsSelector(state)
   };
 }
 
@@ -79,6 +76,5 @@ FolderTreeItem.propTypes = {
   node: PropTypes.shape({
     id: PropTypes.string,
     title: PropTypes.string
-  }),
-  children: PropTypes.arrayOf(PropTypes.element)
+  })
 };
