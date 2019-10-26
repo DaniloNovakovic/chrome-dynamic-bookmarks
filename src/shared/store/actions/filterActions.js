4fdb9c9b108892a33c8c24c8709547a1046b0612
Danilo Novakovic <dakenzi97@gmail.com>
2019-10-26 10:02:50 +0200

    Selected elements are now cleared when new filter is applied (ex. when you open different folder)

--- src/shared/store/actions/filterActions.js
+++ src/shared/store/actions/filterActions.js
@@ -1,5 +1,8 @@
 import { actionTypes } from "shared/constants";
 
 export function applyFilter(filter = {}) {
-  return { type: actionTypes.APPLY_FILTER, filter };
+  return dispatch => {
+    dispatch({ type: actionTypes.APPLY_FILTER, filter });
+    dispatch({ type: actionTypes.CLEAR_SELECTED });
+  };
 }
